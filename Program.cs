dc55bbfae15568cb2fe7db08f892d10b8d518cb1
marius.herciu23 <marius.herciu23@gmail.com>
2022-06-26 16:58:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Ex._1.Magazin_Laborator8_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Scrieți un program care va modela un magazin de electronice. Magazinul de electronice va avea afișate
+             televizoare, telefoane, becuri inteligente precum si sisteme audio. Când clientul va intra in magazin,
+             becurile se vor aprinde, televizoarele vor porni pe un program cu muzică iar telefoanele își vor debloca
+             automat ecranul. Clientul va putea cumpăra produse de la casă iar aceasta va calcula automat veniturile.
+             La plecarea clientului, becurile se vor stinge, televizoarele se vor opri iar telefoanele isi vor bloca
+             ecranul.
+
+
+            Suplimentar
+
+             Telefonul
+             • Va avea o metodă ”deblochează” care va primi ca parametru o parola
+                    o Telefonul va fi deblocat doar dacă parola este corectă sau dacă încă nu a fost setată
+                    (empty string)
+             • Va avea o metodă de schimbare a parolei care va primi doi parametrii, parola veche și parola
+             nouă
+                    o Parola va fi schimbată doar dacă parola veche este corectă
+                    o Va afișa in consola dacă parola a fost sau nu schimbată cu succes
+             • Va persista starea telefonului (blocat/deblocat)
+             • Va expune o metodă ”apelează”
+                    o va primi ca parametru un număr de telefon
+                    o va efectua apelul doar dacă telefonul este deblocat
+
+            Magazinul
+         • Va avea o parolă pe care o va seta automat telefoanelor atunci când un telefon nou va intra în
+         stoc
+         • Va oferi o metodă de schimbare a parolei (aceeași regulă ca la schimbarea parolei telefonului) la
+         apelul căreia, dacă parola veche este corectă, va schimba parola tuturor telefoanelor aflate în
+         stoc
+         • La vânzarea unui telefon, parola acestuia va fi resetată pe valoarea inițiala (empty string) astfel
+         încât cumpărătorul să își poată seta propria parolă
+             */
+
+            Magazin magazin = new Magazin();
+
+            Bec bec = new Bec();
+
+            Telefon telefoniPhone10 = new Telefon(ProducatorTelefon.iPhone, ModelTelefon.x10);
+            Telefon telefonNokia3310 = new Telefon(ProducatorTelefon.Nokia, ModelTelefon.n3310);
+            Telefon telefonOneplusN10 = new Telefon(ProducatorTelefon.OnePlus, ModelTelefon.N10);
+            Telefon telefonSamsungS10 = new Telefon(ProducatorTelefon.Samsung, ModelTelefon.S10);
+
+            TV tvSamsungSA55 = new TV(ProducatorTV.Samsung, ModelTV.SA55);
+            TV tvLG30 = new TV(ProducatorTV.LG, ModelTV.LG30);
+            TV tvnei45 = new TV(ProducatorTV.Nei, ModelTV.N45);
+            TV tvphilipsPH35 = new TV(ProducatorTV.Philips, ModelTV.PH35);
+
+
+            magazin.AdaugareBec(bec);
+            magazin.AdaugareBec(bec);
+            magazin.AdaugareBec(bec);
+            magazin.AdaugareBec(bec);
+            magazin.AdaugareBec(bec);
+
+
+            Telefon telefonSamsungTest = new Telefon(ProducatorTelefon.Samsung, ModelTelefon.S10);
+            telefonSamsungTest.SchimbaParola("0000", "1234");
+            telefonSamsungTest.Deblocheaza("4343");
+            telefonSamsungTest.Apelare("0722222222");
+            telefonSamsungTest.Deblocheaza("1234");
+            telefonSamsungTest.Apelare("0722222222");
+
+            magazin.AdaugareTelefon(telefonSamsungTest);
+
+
+
+            magazin.AdaugareTelefon(telefonOneplusN10);
+            magazin.AdaugareTelefon(telefonNokia3310);
+            magazin.AdaugareTelefon(telefonNokia3310);
+            magazin.AdaugareTelefon(telefonNokia3310);
+            magazin.AdaugareTelefon(telefoniPhone10);
+            magazin.AdaugareTelefon(telefoniPhone10);
+            magazin.AdaugareTelefon(telefonSamsungS10);
+            magazin.AdaugareTelefon(telefonSamsungS10);
+
+            magazin.AdaugareTV(tvSamsungSA55);
+            magazin.AdaugareTV(tvLG30);
+            magazin.AdaugareTV(tvnei45);
+            magazin.AdaugareTV(tvphilipsPH35);
+            magazin.AdaugareTV(tvLG30);
+            magazin.AdaugareTV(tvSamsungSA55);
+
+            magazin.ClientIntra();
+            magazin.SchimbareParolaToateTelefoanele("0000", "9999");
+            magazin.VanzareProduse(magazin);
+            magazin.ClientIese();
+
+        }
+    }
+}
---
+++ TV.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex._1.Magazin_Laborator8_
+{
+    class TV
+    {
+        /*• Fiecare televizor va costa 1000 de ron.
+          • Va avea un model și producător
+          • Va avea o metodă care va porni televizorul pe un program cu muzica și va confirma acest lucru
+          în consolă, menționând modelul și producătorul.
+          • Va avea o metodă care va opri televizorul și va confirma acest lucru în consolă*/
+
+        private int pret = 1000;
+        private ModelTV model;
+        private ProducatorTV producator;
+        private bool pornit = false;
+        private PosturiTV post;
+
+
+        enum PosturiTV
+        {
+            Stiri, Muzica, Cultural, Cartoon
+        }
+        /// <summary>
+        /// Creaza televizor.
+        /// </summary>
+        /// <param name="producator"></param>
+        /// <param name="model"></param>
+        public TV(ProducatorTV producator, ModelTV model)
+        {
+            this.producator = producator;
+            this.model = model;
+        }
+
+
+
+        /// <summary>
+        /// Porneste TV.
+        /// </summary>
+        public void Porneste()
+        {
+            pornit = true;
+            this.post = PosturiTV.Muzica;
+            Console.WriteLine($"{this.producator} {this.model} este pornit pe canalul {this.post}.");
+        }
+        /// <summary>
+        /// Opreste TV.
+        /// </summary>
+        public void Opreste()
+        {
+            pornit = false;
+            Console.WriteLine($"{this.producator} {this.model} este oprit.");
+        }
+        /// <summary>
+        /// Returneaza pretul unui televizor.
+        /// </summary>
+        /// <returns></returns>
+        public int GetPret()
+        {
+            return this.pret;
+        }
+        /// <summary>
+        /// Returneaza numele producatorului unui TV.
+        /// </summary>
+        /// <returns></returns>
+        public ProducatorTV GetProducator()
+        {
+            return this.producator;
+        }
+        /// <summary>
+        /// Returneaza modelul unui TV.
+        /// </summary>
+        /// <returns></returns>
+        public ModelTV GetModel()
+        {
+            return this.model;
+        }
+        /// <summary>
+        /// Schimba un TV pe canalul Muzica.
+        /// </summary>
+        public void SetPostTVMuzica()
+        {
+            this.post = PosturiTV.Muzica;
+        }
+    }
+    enum ProducatorTV
+    {
+        Samsung, LG, Nei, Philips, empty
+    }
+    enum ModelTV
+    {
+        SA55, LG30, N45, PH35, empty
+    }
+
+}
